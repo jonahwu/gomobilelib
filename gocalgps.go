8e362ed97ff4a9130771d6da9089d3648562273f
jonahwu <jonahjun.wu@gmail.com>
2017-07-09 12:59:13 +0800

    adding real calculation

--- gocalgps.go
+++ gocalgps.go
@@ -5,6 +5,7 @@ import (
 	"github.com/tidwall/gjson"
 	//	"time"
 	"encoding/json"
+	"math"
 )
 
 type Gps2dLoc struct {
@@ -23,11 +24,13 @@ type ListGps2dLocTim struct {
 	Timestamp int
 }
 
-type TestInfo struct {
-	Testnum    float64
-	Gpsloc     *Gps2dLoc
-	PrevLoc    Gps4dLoc
-	CurrentLoc Gps4dLoc
+type GLibInfo struct {
+	Gpsloc          *Gps2dLoc // for gmob used for notification
+	Testnum         float64
+	PrevLoc         Gps4dLoc
+	CurrentLoc      Gps4dLoc
+	TargetCameraLoc ListGps2dLocTim
+	CreateLoc       Gps4dLoc
 }
 
 func NewGp2dLoc() *Gps2dLoc {
@@ -35,11 +38,87 @@ func NewGp2dLoc() *Gps2dLoc {
 	return &a
 }
 
-func (tt *TestInfo) InitState() {
+func (tt *GLibInfo) InitState(id string, ts int, locx float64, locy float64) {
 	fmt.Println("show the PrevLoc", tt.PrevLoc)
+
 }
 
-func (tt *TestInfo) ShowData(latidata float64, longdata float64) {
+func (tt *GLibInfo) Start(ts int, locx float64, locy float64) int {
+	fmt.Println("start now", ts)
+	return 0
+
+}
+func (tt *GLibInfo) FilterDistance(tarx float64, tary float64) (float64, int) {
+	locx := tt.CurrentLoc.Lati
+	locy := tt.CurrentLoc.Long
+	distcurr := tt.FilterCalDistance(locx, locy, tarx, tary)
+	locpx := tt.PrevLoc.Lati
+	locpy := tt.PrevLoc.Long
+	distprev := tt.FilterCalDistance(locpx, locpy, tarx, tary)
+	fmt.Println("dist current, dist prev", distcurr, distprev)
+	if distprev < distcurr {
+		return distcurr, 0
+	} else {
+		return distcurr, 1
+	}
+
+}
+
+func (tt *GLibInfo) GLibFilter(ts int, gpsx float64, gpsy float64) (float64, int) {
+	tarx := 25.080223
+	tary := 121.697908
+	tt.UpdateCurrent(ts, gpsx, gpsy)
+	dist, runflag := tt.FilterDistance(tarx, tary)
+	tt.FilterUpdatePrev(ts, gpsx, gpsy)
+	if runflag != 0 {
+		var notiflag int
+		fmt.Println(dist)
+		if dist < 500.0 {
+			notiflag = 1
+		} else {
+			notiflag = 0
+		}
+		return dist, notiflag
+	} else {
+		return dist, 0
+	}
+}
+
+func (tt *GLibInfo) UpdateCurrent(ts int, gpsx float64, gpsy float64) {
+	tt.CurrentLoc.Lati = gpsx
+	tt.CurrentLoc.Long = gpsy
+	tt.CurrentLoc.Timestamp = ts
+}
+
+func (tt *GLibInfo) FilterUpdatePrev(ts int, gpsx float64, gpsy float64) {
+	fmt.Println(ts, tt.PrevLoc.Timestamp)
+	if (ts - tt.PrevLoc.Timestamp) > 5 {
+		tt.PrevLoc.Lati = gpsx
+		tt.PrevLoc.Long = gpsy
+		tt.PrevLoc.Timestamp = ts
+		fmt.Println("update Prev")
+	}
+
+}
+
+func (tt *GLibInfo) FilterCalDistance(locx float64, locy float64, tarx float64, tary float64) float64 {
+	//	fmt.Println("currtt loc", tt.CurrentLoc.Long, tt.CurrentLoc.Lati, tarx, tary)
+	//	fmt.Println("cal distance", CalDistance(tt.CurrentLoc.Lati, tt.CurrentLoc.Long, tarx, tary))
+	return CalDistance(locx, locy, tarx, tary)
+}
+
+func CalDistance(lati1 float64, long1 float64, lati2 float64, long2 float64) float64 {
+	d2r := 0.0174532925199433
+	dlong := (long2 - long1) * d2r
+	dlat := (lati2 - lati1) * d2r
+	a := math.Pow(math.Sin(dlat/2.0), 2) + math.Cos(lati1*d2r)*math.Cos(lati2*d2r)*math.Pow(math.Sin(dlong/2.0), 2)
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+	d := 6373 * c
+	//return meter
+	return d * 1000.
+}
+
+func (tt *GLibInfo) ShowData(latidata float64, longdata float64) {
 	//	if tt
 	fmt.Println("initial testnum", tt.Testnum)
 	fmt.Println("testnum put in client", tt.Testnum)
@@ -48,30 +127,15 @@ func (tt *TestInfo) ShowData(latidata float64, longdata float64) {
 	tt.Testnum = 77.88
 	fmt.Println("show Gpsloc", tt.Gpsloc)
 }
-func (tt *TestInfo) GetData() float64 {
+func (tt *GLibInfo) GetData() float64 {
 
 	return tt.Testnum
 
 }
 
-type UserGpsInfo struct {
-	CurrentLoc      Gps4dLoc
-	CameraLoc       ListGps2dLocTim
-	TargetCameraLoc ListGps2dLocTim
-	// > 5 sec to update
-	PrevLoc Gps4dLoc
-	// as map filter used
-	CreateLoc Gps4dLoc
-}
-
-func NewCamera() *TestInfo {
-	a := &TestInfo{}
+func NewGLib() *GLibInfo {
+	a := &GLibInfo{}
 	return a
-}
-
-func (f *UserGpsInfo) DetectCamera() {
-	fmt.Println("into struct detectcamera")
-	return
 }
 
 func CalGpsDistance(a string) string {
