3bf9e41a12375f6aae99c9c2ce126e2119572b9b
jonahwu <jonahjun.wu@gmail.com>
2017-07-13 06:15:56 +0800

    adding beepornot to filter beep in the range of distance from greater than 100

--- gocalgps.go
+++ gocalgps.go
@@ -19,10 +19,20 @@ type Gps2dLocVel struct {
 }
 
 type NearCamLocVel struct {
-	Distance  float64
-	Vel       int
-	Timestamp int
-	Flag      int
+	Distance    float64
+	Vel         int
+	Timestamp   int
+	Flag        int
+	OldDistance float64
+}
+
+type OldNearCamLocVel struct {
+	Distance       float64
+	Vel            int
+	Timestamp      int
+	Flag           int
+	OldDistance    float64
+	DenoiseCounter int
 }
 
 type Gps4dLoc struct {
@@ -37,13 +47,14 @@ type ListGps2dLocTim struct {
 }
 
 type GLibInfo struct {
-	Gpsloc          *Gps2dLoc // for gmob used for notification
-	Testnum         float64
-	PrevLoc         Gps4dLoc
-	CurrentLoc      Gps4dLoc
-	TargetCameraLoc ListGps2dLocTim
-	CreateLoc       Gps4dLoc
-	NearestCamera   NearCamLocVel
+	Gpsloc           *Gps2dLoc // for gmob used for notification
+	Testnum          float64
+	PrevLoc          Gps4dLoc
+	CurrentLoc       Gps4dLoc
+	TargetCameraLoc  ListGps2dLocTim
+	CreateLoc        Gps4dLoc
+	NearestCamera    NearCamLocVel
+	OldNearestCamera OldNearCamLocVel
 }
 
 func NewGp2dLoc() *Gps2dLoc {
@@ -57,6 +68,7 @@ func (tt *GLibInfo) InitState(id string, ts int, locx float64, locy float64) {
 
 	// set up fake camra
 	ta := Gps2dLocVel{}
+
 	ta.Lati = 25.080223
 	ta.Long = 121.697908
 	ta.Vel = 50
@@ -78,14 +90,169 @@ func (tt *GLibInfo) InitState(id string, ts int, locx float64, locy float64) {
 	ta.Vel = 50
 	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
 
+	ta.Lati = 25.087720
+	ta.Long = 121.695398
+	ta.Vel = 50
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.101782
+	ta.Long = 121.696034
+	ta.Vel = 50
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.121502
+	ta.Long = 121.697590
+	ta.Vel = 80
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.121832
+	ta.Long = 121.697869
+	ta.Vel = 80
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.092500
+	ta.Long = 121.699440
+	ta.Vel = 50
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.080700
+	ta.Long = 121.701497
+	ta.Vel = 50
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.090313
+	ta.Long = 121.706649
+	ta.Vel = 50
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.097341
+	ta.Long = 121.716191
+	ta.Vel = 50
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.103060
+	ta.Long = 121.725280
+	ta.Vel = 50
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.111110
+	ta.Long = 121.735000
+	ta.Vel = 50
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.102713
+	ta.Long = 121.754820
+	ta.Vel = 80
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.102885
+	ta.Long = 121.755578
+	ta.Vel = 80
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.059990
+	ta.Long = 121.640210
+	ta.Vel = 50
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.061110
+	ta.Long = 121.642590
+	ta.Vel = 50
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.061570
+	ta.Long = 121.651350
+	ta.Vel = 50
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.066710
+	ta.Long = 121.664570
+	ta.Vel = 50
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.076990
+	ta.Long = 121.671550
+	ta.Vel = 50
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
 	ta.Lati = 25.065942
 	ta.Long = 121.674905
-	ta.Vel = 90
+	ta.Vel = 50
 	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
 
 	ta.Lati = 25.068420
 	ta.Long = 121.681260
-	ta.Vel = 90
+	ta.Vel = 50
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.072780
+	ta.Long = 121.683330
+	ta.Vel = 50
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+	//大安區
+	ta.Lati = 25.057780
+	ta.Long = 121.532220
+	ta.Vel = 50
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.017220
+	ta.Long = 121.533330
+	ta.Vel = 50
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.017220
+	ta.Long = 121.533330
+	ta.Vel = 50
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.030830
+	ta.Long = 121.533330
+	ta.Vel = 50
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.041670
+	ta.Long = 121.536940
+	ta.Vel = 50
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.025280
+	ta.Long = 121.537780
+	ta.Vel = 70
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.027892
+	ta.Long = 121.537864
+	ta.Vel = 50
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.044170
+	ta.Long = 121.540560
+	ta.Vel = 40
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.021670
+	ta.Long = 121.543330
+	ta.Vel = 50
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.024440
+	ta.Long = 121.548610
+	ta.Vel = 50
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.021670
+	ta.Long = 121.548890
+	ta.Vel = 60
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.021670
+	ta.Long = 121.548890
+	ta.Vel = 60
+	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
+
+	ta.Lati = 25.017220
+	ta.Long = 121.549170
+	ta.Vel = 50
 	tt.TargetCameraLoc.Loc = append(tt.TargetCameraLoc.Loc, ta)
 
 	tt.TargetCameraLoc.Timestamp = ts
@@ -105,16 +272,55 @@ func (tt *GLibInfo) InitState(id string, ts int, locx float64, locy float64) {
 func (tt *GLibInfo) UpdateCamera() {
 }
 
+func (tt *GLibInfo) BeepOrNot(nc NearCamLocVel) bool {
+	if nc.Flag == 0 {
+		regionBeep := false
+		if tt.OldNearestCamera.DenoiseCounter >= 2 {
+			tt.OldNearestCamera.Timestamp = nc.Timestamp
+			tt.OldNearestCamera.Distance = 0
+			tt.OldNearestCamera.DenoiseCounter = 0
+		}
+		if nc.Timestamp == 0 && tt.OldNearestCamera.Timestamp != 0 {
+			tt.OldNearestCamera.DenoiseCounter = tt.OldNearestCamera.DenoiseCounter + 1
+		}
+		return regionBeep
+	} else {
+
+		if nc.Distance < 100 {
+			regionBeep := true
+			return regionBeep
+		}
+		fmt.Println("into calculate Region Alarm")
+		fmt.Println(tt.OldNearestCamera.Distance, nc.Distance)
+		regionBeep := tt.RegionAlarm(tt.OldNearestCamera.Distance, nc.Distance)
+		fmt.Println(regionBeep)
+		if nc.Timestamp != 0 && tt.OldNearestCamera.Timestamp == 0 {
+			regionBeep = true
+		}
+		if regionBeep {
+			tt.OldNearestCamera.Distance = nc.Distance
+			tt.OldNearestCamera.Flag = nc.Flag
+			tt.OldNearestCamera.Timestamp = nc.Timestamp
+		}
+		return regionBeep
+
+	}
+	return false
+}
+
 //func (tt *GLibInfo) Start(ts int, locx float64, locy float64) (float64, int, int) {
 func (tt *GLibInfo) Start(ts int, locx float64, locy float64) string {
 	tt.UpdateCamera()
 	NearestCamera := tt.GLibFilter(ts, locx, locy)
+
 	fmt.Println("start now", ts)
+	beep := tt.BeepOrNot(NearestCamera)
 	//	vel := NearestCamera.Vel
 	jm := make(map[string]interface{})
 	jm["dist"] = NearestCamera.Distance
 	jm["flag"] = NearestCamera.Flag
 	jm["vel"] = NearestCamera.Vel
+	jm["beep"] = beep
 	ret, _ := json.Marshal(jm)
 	//return dist, flag, vel
 	return string(ret)
@@ -140,7 +346,7 @@ func (tt *GLibInfo) FilterDistance() NearCamLocVel {
 
 	fmt.Println("show near camera", tt.NearestCamera.Distance, tt.NearestCamera.Vel)
 	for i := 0; i < len(tt.TargetCameraLoc.Loc); i++ {
-		fmt.Println("-----------calculate on camera:", i)
+		//fmt.Println("-----------calculate on camera:", i)
 		tarx := tt.TargetCameraLoc.Loc[i].Lati
 		tary := tt.TargetCameraLoc.Loc[i].Long
 
@@ -150,7 +356,7 @@ func (tt *GLibInfo) FilterDistance() NearCamLocVel {
 		locpx := tt.PrevLoc.Lati
 		locpy := tt.PrevLoc.Long
 		distprev := tt.FilterCalDistance(locpx, locpy, tarx, tary)
-		fmt.Println("dist current, dist prev", distcurr, distprev)
+		//fmt.Println("dist current, dist prev", distcurr, distprev)
 		// where 30 is faraway distance
 		if distcurr < 50.0 {
 			//return distcurr, 0
@@ -217,6 +423,22 @@ func (tt *GLibInfo) FilterCalDistance(locx float64, locy float64, tarx float64, 
 	//	fmt.Println("currtt loc", tt.CurrentLoc.Long, tt.CurrentLoc.Lati, tarx, tary)
 	//	fmt.Println("cal distance", CalDistance(tt.CurrentLoc.Lati, tt.CurrentLoc.Long, tarx, tary))
 	return CalDistance(locx, locy, tarx, tary)
+}
+
+// related to beep while dr1=200 dr2=150 not alram but dr2<=100 will alarm
+// same as 90, 85 will not alarm, <80 will alarm
+func (tt *GLibInfo) RegionAlarm(drold float64, drnew float64) bool {
+	//_ := math.Log10(drold)
+	b := math.Log10(drold)
+	dr := math.Abs(drnew - drold)
+	ldr := math.Log10(dr)
+	fmt.Println("drold and ldr", b, ldr, int(b), int(ldr))
+	if int(b) == int(ldr) {
+		return true
+	} else {
+		return false
+	}
+	return false
 }
 
 func CalDistance(lati1 float64, long1 float64, lati2 float64, long2 float64) float64 {
